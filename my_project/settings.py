aa0ff45918c3c87e020fd82f79087c38fb6356bf
jmaldanat <jmaldanat@unal.edu.co>
2025-06-27 09:03:11 -0500

    Add Procfile for Gunicorn deployment and update settings for production

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d+js2lmy1@1ath0ua0(gpb_f95$584-sv#esa@^xa@8%s51x%('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
